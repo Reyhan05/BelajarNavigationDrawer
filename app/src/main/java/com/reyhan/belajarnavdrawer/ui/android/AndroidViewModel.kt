fc1ebdff7cde694ad18f2a3441bcd092882dd9ec
reyhankelas7bputrabukyayah <reyhanmohamad0212@gmail.com>
2022-08-30 13:33:55 +0700

    Finish view model in android

--- app/src/main/java/com/reyhan/belajarnavdrawer/ui/android/AndroidViewModel.kt
+++ app/src/main/java/com/reyhan/belajarnavdrawer/ui/android/AndroidViewModel.kt
@@ -1,3 +1,12 @@
 package com.reyhan.belajarnavdrawer.ui.android
 
-class AndroidViewModel()
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AndroidViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "Android"
+    }
+    val text: LiveData<String> = _text
+}
